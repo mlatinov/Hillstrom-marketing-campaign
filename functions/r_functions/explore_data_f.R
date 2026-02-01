e11c1a5e07ed438c0c545f38a61f00b4222708c6
mlatinov <latinovmetodi@gmail.com>
2026-02-02 00:19:24 +0200

    finallize the eda exploration function

--- functions/r_functions/explore_data_f.R
+++ functions/r_functions/explore_data_f.R
@@ -1,6 +1,6 @@
 
 #### Function to Perform Exploratory Data Analysis ####
-explore_data <- function(clean_data){
+explore_data <- function(data_clean){
 
   #### Source Functions helper
   source("functions/r_helpers.R")
@@ -11,28 +11,61 @@ explore_data <- function(clean_data){
 
   #### Bivariate Analysis ####
 
-  #### Multivariate Analysis ####
+  ## Spend ~ F
+  biv_spend_zipcode <- biv_continuous_factor(
+    data = data_clean,
+    factor = "zip_code",
+    continuous_var = "spend"
+  )
 
+  biv_spend_channel <- biv_continuous_factor(
+    data = data_clean,
+    factor = "channel",
+    continuous_var = "spend"
+  )
 
+  biv_spend_gender <- biv_continuous_factor(
+    data = data_clean,
+    factor = "gender",
+    continuous_var = "spend"
+  )
 
+  ## Visit ~ F
+  biv_visit_channel <- biv_factor_factor(
+    data = data_clean,
+    factor_one = "channel",
+    factor_two = "visit"
+  )
 
+  biv_visit_newbie <- biv_factor_factor(
+    data = data_clean,
+    factor_one = "newbie",
+    factor_two = "visit"
+  )
 
+  biv_visit_gender <- biv_factor_factor(
+    data = data_clean,
+    factor_one = "visit",
+    factor_two = "gender"
+  )
 
+  biv_visit_zip_code <- biv_factor_factor(
+    data = data_clean,
+    factor_one = "zip_code",
+    factor_two = "visit"
+  )
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  #### Return ###
+  return(list(
+    univariate_num_analysis = list(
+      spend_var = uni_spend,
+      history_var = uni_history
+    ),
+    bivariate_analysis = list(
+      biv_visit_channel = biv_visit_channel,
+      biv_visit_newbie = biv_visit_newbie,
+      biv_visit_gender = biv_visit_gender,
+      biv_visit_zip_code = biv_visit_zip_code
+    )
+  ))
 }
